6c6fab55f1c534a0b99c7cb0176f2702b974d73d
204070 <talk2seun2010@hotmail.com>
2018-02-21 16:15:52 +0100

    Added template rendering

--- minimal.py
+++ minimal.py
@@ -1,22 +1,40 @@
 import sys
+from os import getcwd
 
 from django.conf import settings
-from django.conf.urls import url
+from django.urls import path
 from django.core.management import execute_from_command_line
 from django.http import HttpResponse
+from django.template.loader import render_to_string
 
 settings.configure(
     DEBUG=True,
     SECRET_KEY='A-random-secret-key!',
     ROOT_URLCONF=sys.modules[__name__],
+    TEMPLATES=[
+        {
+            'BACKEND': 'django.template.backends.django.DjangoTemplates', 
+            'DIRS': [
+                getcwd()
+                ],
+            },
+        ]
 )
 
 
 def index(request):
-    return HttpResponse('<h1>A minimal Django response!</h1>')
+    return HttpResponse("""<p><a href="./about/">About</a></p> 
+            <h1>Minimal Django!</h1>""")
+
+def about(request):
+    title = 'Tinyapp'
+    author = 'Akinwunmi Oluwaseun'
+    html = render_to_string('about.html', {'title':title, 'author': author,})
+    return HttpResponse(html)
 
 urlpatterns = [
-    url(r'^$', index),
+    path('', index, name='index'),
+    path('about/', about, name='about'),
 ]
 
 if __name__ == '__main__':
